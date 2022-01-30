c45e492117f5e9c9d3837aa87d48df6cf74cc0a7
Nitin <nsinghboss02@gmail.com>
2022-01-30 11:50:03 +0530

    Initial commit

---
+++ src/com/Recursion/Main.java
@@ -0,0 +1,50 @@
+package com.Recursion;
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+	// merge sort with complexity of o(nlogn);.
+        int[] arr = {0,1,1,1,0,0,1};
+        System.out.println(Arrays.toString(mergesort(arr)));
+
+    }
+    public static int[] mergesort(int[] arr){
+        if(arr.length==1){
+            return arr;
+        }
+        int mid = arr.length/2;
+        int[] left = mergesort(Arrays.copyOfRange(arr,0,mid));
+        int[] right = mergesort(Arrays.copyOfRange(arr,mid,arr.length));
+        return merge(left,right);
+
+    }
+    public static int[] merge(int[] first,int[] second){
+        int[] arrFinal = new int[first.length+ second.length];
+        int i=0;
+        int j=0;
+        int k=0;
+
+        while(i< first.length && j< second.length) {
+            if (first[i] > second[j]) {
+                arrFinal[k] = second[j];
+                j++;
+            }
+            else {
+                arrFinal[k] = first[i];
+                i++;
+            }
+            k++;
+        }
+        while(i< first.length){
+            arrFinal[k] = first[i];
+            i++;
+            k++;
+        }
+        while (j< second.length){
+            arrFinal[k] = second[j];
+            j++;
+            k++;
+        }
+        return arrFinal;
+    }
+}
